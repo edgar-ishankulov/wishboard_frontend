6daeb46311d2a7567ef9df019612f87f615aa3cc
edgar-ishankulov <edgar.ishankulov@gmail.com>
2022-06-09 10:16:15 +0300

    created header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,13 @@
 import React from "react"
 import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
 import Container from "react-bootstrap/Container";
 
 const Header = ({title}) => {
     return (
         <Navbar bg="light" variant="light">
-    <Container>
+    <Container fluid className="justify-content-center mt-4">
     <Navbar.Brand href="#home">{title}</Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
+    
     </Container>
   </Navbar>
 
