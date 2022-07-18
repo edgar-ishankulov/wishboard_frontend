80964a3d208a08348860a96e1da08693e20858bc
edgar-ishankulov <edgar.ishankulov@gmail.com>
2022-07-18 20:07:35 +0300

    bug fixws

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
   build: {
-    outDir: 'build',
+    outDir: 'dist',
     manifest: true
   },
 
@@ -27,7 +27,7 @@ export default defineConfig({
       $: 'jquery',
     }),
     react(),
-    VitePWA({ registerType: 'autoUpdate' }),
+    VitePWA({ registerType: 'autoUpdate', injectRegister: 'auto' }),
     svgrPlugin({
       svgrOptions: {
         icon: true,
